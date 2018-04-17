b00d583e96d19b6fc7c07fbe75424cda36714553
LordElf <shawnlai.cn@gmail.com>
2018-04-17 19:22:05 +0800

    stack

---
+++ 2/main.c
@@ -0,0 +1,136 @@
+#include "stack.c"
+#include <stdlib.h>
+#include <stdio.h>
+
+void manipulateO(void);
+void manipulateL(void);
+
+int main() {
+	int i, m=1;
+	while (m == 1) {
+		printf("stack operation\n");
+		printf("-------------------------------\n");
+		printf("1. init an order stack\n2. init a linked stack\n3. exit\n");
+		printf("please input a NO\n");
+		scanf("%d", &i);
+		switch (i) {
+		case 1:
+			manipulateO();
+			break;
+		case 2:
+			manipulateL();
+			break;
+		case 3:
+			m = 0;
+			break;
+		}
+	}
+	return 0;
+}
+
+void manipulateO(void) {
+	int size;
+	SqStack* s = (SqStack*)malloc(sizeof(SqStack));
+	printf("Plz input the size of stack: ");
+	scanf("%d", &size);
+	if (ERROR == initStack(s,size))
+		return;
+	int i;
+	for (;;) {
+		printf("\nWhat do you want to mainpulate this OrderStack?\n");
+		printf("------------------------------------------------\n");
+		printf("1. isEmptyStack\n2. getTopStack\n3. clearStack\n4.destoryStack\n5.stackLength\n6.pushStack\n");
+		printf("7.popStack \n8.exit\n");
+		printf("please input a NO");
+		scanf("%d", &i);
+		switch (i) {
+			case 1:
+				isEmptyStack(s) == OK ? printf("YES\n") : printf("NO\n");
+				break;
+			case 2:
+				;
+				ElemType *top = (ElemType* )malloc(sizeof(ElemType));
+				getTopStack(s, top) == OK ? printf("OK\n") : printf("ERROR\n");
+				printf("top element is %d", *top);
+				break;
+			case 3:
+				clearStack(s) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 4:
+				destroyStack(s) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 5:
+				;
+				int length = 0;
+				stackLength(s, length) == OK ? printf("OK\n") : printf("ERROR\n");
+				printf("length of the stack is %d", length);
+				break;
+			case 6:
+				;
+				ElemType data;
+				printf("input data U want to push into the stack");
+				scanf("%d", &data);
+				pushStack(s,data) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 7:
+				;
+				ElemType* datas = (ElemType* )malloc(sizeof(ElemType));
+				popStack(s, datas) == OK ? printf("OK\n") : printf("ERROR\n");
+				printf("the element just poped out is %d", *datas);
+				break;
+			case 8:
+				return;
+		}
+	}
+}
+
+void manipulateL(void) {
+	int i;
+	LinkStack* s = (LinkStack*)malloc(sizeof(LinkStack));
+	for (;;) {
+		printf("\nWhat do you want to mainpulate this LinkStack?\n");
+		printf("----------------------------------------------\n");
+		printf("1. isEmptyLStack\n2. getTopLStack\n3. clearLStack\n4. destroyLStack\n5. LStackLength\n");
+		printf("6. pushLStack\n7. popLStack\n8. exit\n");
+		printf("please input a NO");
+		scanf("%d", &i);
+
+		switch (i) {
+			case 1:
+				isEmptyLStack(s) == OK ? printf("YES\n") : printf("NO\n");
+				break;
+			case 2:
+				;
+				ElemType* top = (ElemType* )malloc(sizeof(ElemType));
+				getTopLStack(s, top) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 3:
+				clearLStack(s) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 4:
+				destroyLStack(s) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 5:
+				;
+				int* length = (int*)malloc(sizeof(int));
+				LStackLength(s, length) == OK ? printf("OK\n") : printf("ERROR\n");
+				printf("length of the stack is %d", *length);
+				break;
+			case 6:
+				;
+				ElemType data;
+				printf("input data U want to push into the stack");
+				scanf("%d", &data);
+				pushLStack(s, data) == OK ? printf("OK\n") : printf("ERROR\n");
+				break;
+			case 7:
+				;
+				ElemType* datas = (ElemType* )malloc(sizeof(ElemType));
+				popLStack(s, datas) == OK ? printf("OK\n") : printf("ERROR\n");
+				printf("the element just poped out is %d", *datas);
+				break;
+			case 8:
+			return;
+		}
+	}
+}
---
+++ 2/stack.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+//顺序栈(基于数组的)
+Status initStack(SqStack *s,int sizes)  //初始化链表
+{
+	s->elem = (ElemType *)malloc(sizes * sizeof(ElemType));
+	if (NULL == s)
+		return ERROR;
+	s->top = -1;
+	s->size = sizes;
+	return OK;
+}
+
+
+Status isEmptyStack(SqStack *s)   //判断链表是否为空
+{
+	return (-1 == s->top ? OK : ERROR);
+}
+
+
+Status getTopStack(SqStack *s,ElemType *e)   //得到链表头元素
+{
+	if (NULL == s || -1 == s->top)
+		return ERROR;
+	else
+		*e = s->elem[s->top];
+	return OK;
+}
+
+
+Status clearStack(SqStack *s)   //清空链表
+{
+    if(NULL == s)
+        return ERROR;
+	s->top = -1;
+    return OK;
+}
+
+
+Status destroyStack(SqStack *s)  //销毁链表
+{
+	if (NULL == s)
+		return ERROR;
+	free(s->elem);
+	return OK;
+}
+
+
+Status stackLength(SqStack *s,int length)   //检测链表长度
+{
+	if(NULL == s)
+		return ERROR;
+	length = s->top + 1;
+	return OK;
+}
+
+Status pushStack(SqStack *s,ElemType datas)  //入栈
+{
+	if (s->top >= s->size - 1 || NULL == s)
+		return ERROR;
+	s->top++;
+    s->elem[s->top] = datas;
+	return OK;
+}
+
+Status popStack(SqStack *s,ElemType *datas)   //出栈
+{
+	if (NULL == s || -1 == s->top)
+		return ERROR;
+	*datas = s->elem[s->top];
+	s->top--;
+	return OK;
+}
+
+//链栈(基于链表的)
+Status initLStack(LinkStack *s)   //初始化
+{
+	s->top = (LinkStackPtr)malloc(sizeof(StackNode));
+	if(NULL == s->top)
+		return ERROR;
+	s->count = 0;
+	s->top->next=NULL;
+	return OK;
+}
+
+Status isEmptyLStack(LinkStack *s)  //判断链表是否为空
+{
+	return(NULL == s->top ? OK : ERROR);
+}
+
+Status getTopLStack(LinkStack *s,ElemType *e)  //得到链表头元素
+{
+	if(NULL == s || NULL == s->top)
+		return ERROR;
+    LinkStackPtr p = s->top;
+	for(; p->next != NULL; p = p->next);
+	*e = p->data;
+	return OK;
+}
+
+Status clearLStack(LinkStack *s)   //清空链表
+{
+	return (destroyLStack(s) && initLStack(s) ? OK : ERROR);
+}
+
+Status destroyLStack(LinkStack *s)   //销毁链表
+{
+	if(NULL == s)
+		return ERROR;
+    LinkStackPtr tmp = (LinkStackPtr)malloc(sizeof(StackNode));
+	for(LinkStackPtr p = s->top ; s->count != 0 ; *tmp = *p, p = p->next, free(tmp), s->count--);
+	return OK;
+}
+
+Status LStackLength(LinkStack *s,int *length)    //检测链表长度
+{
+	if(NULL == s)
+		return ERROR;
+	*length = s->count;	
+	return OK;
+}
+
+Status pushLStack(LinkStack *s,ElemType datas)   //入栈
+{
+	if(NULL == s || NULL == s->top)
+		return ERROR;
+	s->count++;
+	LinkStackPtr p = (LinkStackPtr)malloc(sizeof(StackNode));
+	p->next = s->top;
+	p->data = datas;
+	s->top = p;
+	return OK;
+}
+
+Status popLStack(LinkStack *s,ElemType *datas)   //出栈
+{
+	if(NULL == s || NULL == s->top)
+		return ERROR;
+	*datas = s->top->data;
+	s->count--;
+	LinkStackPtr tmp = s->top;
+	s->top = s->top->next;
+	free(tmp);
+	return OK;
+}
